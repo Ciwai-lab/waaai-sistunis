2387335bade3b98bf578aa0bf0cc83865118724d
CiwAI Smart Digital <ciwaismartdigital@gmail.com>
2025-11-05 21:41:47 +0700

    Fix: Sync DB_NAME and DB_PASS names in server.js

--- sistunis-backend/server.js
+++ sistunis-backend/server.js
@@ -36,8 +36,8 @@ app.use(cors());         // Mengizinkan akses dari domain luar (frontend)
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
     port: 5432,
 });
 
